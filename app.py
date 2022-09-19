951cf0e97a09fea8f78f6e9143f7dbc3a8b2036a
Sebastian Yactayo <syactayocrisostomo@gmail.com>
2022-09-18 20:24:28 -0500

    refactored code in db, models and routes

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from dotenv import load_dotenv
+import uvicorn
+import os
 
 from routes.auth_routes import auth_routes
 from routes.account_routes import account_routes
@@ -18,3 +20,7 @@ app.add_middleware(
 
 app.include_router(auth_routes, prefix="/api/auth")
 app.include_router(account_routes, prefix="/api/account")
+
+
+if __name__ == "__main__":
+    uvicorn.run("app:app", host="0.0.0.0", port=os.getenv("PORT", default=5000), log_level="info")
